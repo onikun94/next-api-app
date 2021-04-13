952938567ff0c0d78993b0a67cb46684b152eed6
onikun94 <onikun94@gmail.com>
2021-04-13 18:02:56 +0900

    Add Button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
-import next from "next";
+import { Button } from "../components/Button";
 import Link from "next/link";
+import Head from "next/head";
 const Home = () => {
   return (
     <div>
+      <Head>
+        <title>HELLO ONIKUN</title>
+      </Head>
       <h1>main page</h1>
       <h2>I will implement various page by using webAPI</h2>
+      <Button />
       <ul>
         <li>
           <Link href="posts/first-post">
