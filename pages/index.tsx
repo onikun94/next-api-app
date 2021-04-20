b361224e478f70655a2aa0c26ef7e858c82b4c78
onikun94 <onikun94@gmail.com>
2021-04-21 03:21:04 +0900

    try some css

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,29 @@ const Home = () => {
           </Link>
         </li>
       </ul>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">1</th>
+            <th scope="col">2</th>
+            <th scope="col">3</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
     </Layout>
   );
 };
